345962d645fe34c3293f32bb6cc8af5bf5655603
Luis Millan <luis.millan@gmail.com>
2021-10-20 01:24:01 -0500

    test post

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("hello world");
-  res.status(200).json({ message: "hello world" });
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  res.status(200).send(req.body.challenge);
 });
 
 app.listen(process.env.PORT || 3000);
