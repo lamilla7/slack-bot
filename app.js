9b55a2f94957fdf2e342663f468beac5f0871a04
Luis Millan <luis.millan@gmail.com>
2021-10-21 13:14:54 -0500

    missing conf

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const router = express.Router();
+
+const allowCrossDomain = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
+};
 
 async function main() {
   const app = express();
-
+  app.use(cors());
+  app.use(allowCrossDomain);
   app.use(bodyParser.json());
   app.use(router);
 
